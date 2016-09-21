faf2d14c9bdc7cbf3bbf10a825d0061c99a78f53
CristianC96 <cristian_alejandro_cv@hotmail.com>
2016-09-21 10:47:54 -0500

    Update factorial.py

--- PYTHON/factorial.py
+++ PYTHON/factorial.py
@@ -1,8 +1,11 @@
 def factorial(numero)
  print numero
 if numero==0||numero==1:
-  return 1
+  
+  
+  
+  drtgklñhjnñreturn 1
  else:
-  return numero + factorial(numero-2)
+  return numerotrjirtujtrujtruj + factorial(numero-2)
   
   factorial(12)
